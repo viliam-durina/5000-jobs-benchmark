6a1a6b5e21d9295913a185040d4b8271295717c0
Viliam Durina <viliam@hazelcast.com>
2017-05-25 14:42:32 +0200

    Initial version

---
+++ src/main/java/FiveKJobs.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.hazelcast.core.IAtomicLong;
+import com.hazelcast.jet.AggregateOperation;
+import com.hazelcast.jet.DAG;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.ProcessorMetaSupplier;
+import com.hazelcast.jet.Processors;
+import com.hazelcast.jet.TimestampKind;
+import com.hazelcast.jet.TimestampedEntry;
+import com.hazelcast.jet.Vertex;
+import com.hazelcast.jet.WindowDefinition;
+import com.hazelcast.jet.function.DistributedFunction;
+import com.hazelcast.nio.IOUtil;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static com.hazelcast.jet.AggregateOperations.averagingLong;
+import static com.hazelcast.jet.Edge.between;
+import static com.hazelcast.jet.Processors.writeFile;
+import static com.hazelcast.jet.PunctuationPolicies.withFixedLag;
+import static com.hazelcast.jet.Util.entry;
+import static com.hazelcast.jet.WindowDefinition.slidingWindowDef;
+import static com.hazelcast.jet.WindowingProcessors.combineToSlidingWindow;
+import static com.hazelcast.jet.WindowingProcessors.groupByFrameAndAccumulate;
+import static com.hazelcast.jet.WindowingProcessors.insertPunctuation;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class FiveKJobs {
+
+    private static final long BENCHMARK_TIMEOUT = SECONDS.toMillis(1800);
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("hazelcast.logging.type", "log4j");
+
+        if (args.length != 4) {
+            System.err.println("Usage:");
+            System.err.println("  " + FiveKJobs.class.getSimpleName()
+                    + " <numJobs> <globalItemsPerJob> <isCooperative> <numHeavyJobs(_=all)>");
+            System.exit(1);
+        }
+
+        /* Number of jobs spawn */
+        final int numJobs = Integer.parseInt(args[0]);
+        /* Items emitted per job per second, cluster-wide */
+        final int globalItemsPerSecondPerJob = Integer.parseInt(args[1]);
+        final boolean cooperative = Boolean.parseBoolean(args[2]);
+        final int heavyJobs = args[3].equals("_") ? Integer.MAX_VALUE : Integer.parseInt(args[3]);
+
+        Path directory = Files.createTempDirectory(FiveKJobs.class.getSimpleName());
+        String sDirectory = directory + File.separator;
+
+        JetInstance instance = Jet.newJetInstance();
+//        JetInstance instance2 = Jet.newJetInstance();
+
+//        ClientConfig config = new ClientConfig();
+//        config.getNetworkConfig().setAddresses(singletonList("10.212.1.101"));
+//        config.getGroupConfig().setName("jet");
+//        config.getGroupConfig().setPassword("jet-pass");
+//        JetInstance instance = Jet.newJetClient(config);
+
+//        JetInstance instance = JetBootstrap.getInstance();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtil.delete(directory.toFile())));
+
+        try {
+            int clusterSize = instance.getCluster().getMembers().size();
+            ExecutorService executor = Executors.newFixedThreadPool(20);
+            for (int i = 0; i < numJobs; i++) {
+                int finalI = i;
+                executor.submit(() -> {
+                    instance.newJob(buildDag(sDirectory + finalI, clusterSize, "lag" + finalI,
+                            finalI < heavyJobs ? 0 : 10000,
+                            finalI < heavyJobs ? globalItemsPerSecondPerJob / clusterSize : 100, cooperative)).execute();
+                    System.out.println("job " + finalI + " submitted");
+                });
+            }
+            executor.shutdown();
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+
+            IAtomicLong lagTracker = instance.getHazelcastInstance().getAtomicLong("lag0");
+            for (int i = 0; i < BENCHMARK_TIMEOUT / 1000; i++) {
+                System.out.println("1st job emission lag: " + lagTracker.get());
+                Thread.sleep(1000);
+            }
+        } finally {
+            Jet.shutdownAll();
+        }
+    }
+
+    private static DAG buildDag(String directory, int clusterSize, String lagTrackerName, int minSleepTime,
+                                int itemsPerSecond, boolean cooperative) {
+        DAG dag = new DAG();
+
+        DistributedFunction<Entry<Long, Integer>, Integer> keyExtractor =
+                e -> Math.floorMod(e.getValue(), clusterSize * 10);
+        WindowDefinition wDef = slidingWindowDef(10_000, 1_000);
+        AggregateOperation<Entry<Long, Integer>, ?, Double> aggrOper = averagingLong(Entry::getValue);
+
+        // All processors have local parallelism of 1: with so many expected
+        // jobs, the parallelization will be achieved between jobs. This also
+        // reduces per-job overhead as the number of queues between processors
+        // is lower.
+        Vertex source = dag.newVertex("source",
+                () -> new RandomDataP(itemsPerSecond, cooperative, lagTrackerName, minSleepTime))
+                           .localParallelism(1);
+        Vertex insertPunc = dag.newVertex("insertPunc",
+                insertPunctuation(Entry<Long, Integer>::getKey, () -> withFixedLag(0).throttleByFrame(wDef)))
+                .localParallelism(1);
+        Vertex slidingWindowStage1 = dag.newVertex("slidingWindowStage1",
+                groupByFrameAndAccumulate(keyExtractor, Entry::getKey, TimestampKind.EVENT, wDef, aggrOper))
+                .localParallelism(1);
+        Vertex slidingWindowStage2 = dag.newVertex("slidingWindowStage2",
+                combineToSlidingWindow(wDef, aggrOper))
+                .localParallelism(1);
+        Vertex mapToLatency = dag.newVertex("mapToLatency",
+                Processors.map((TimestampedEntry e) -> entry(e.getKey(), System.currentTimeMillis() - e.getTimestamp())))
+                .localParallelism(1);
+        Vertex sink = dag.newVertex("sink", !cooperative ? writeFile(directory) : ProcessorMetaSupplier.of(Processors.noop()))
+                .localParallelism(1);
+
+        dag.edge(between(source, insertPunc).oneToMany())
+           .edge(between(insertPunc, slidingWindowStage1).oneToMany())
+           .edge(between(slidingWindowStage1, slidingWindowStage2)
+                   .distributed())
+           .edge(between(slidingWindowStage2, mapToLatency).oneToMany())
+           .edge(between(mapToLatency, sink).oneToMany());
+
+        return dag;
+    }
+}
---
+++ src/main/java/RandomDataP.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.hazelcast.core.IAtomicLong;
+import com.hazelcast.jet.AbstractProcessor;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+import java.util.concurrent.locks.LockSupport;
+
+import static com.hazelcast.jet.Util.entry;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class RandomDataP extends AbstractProcessor {
+
+    private final int itemsPerSecond;
+    private final boolean cooperative;
+    private final String lagTrackerName;
+
+    private Random random = new Random();
+    private long startNanoTime;
+    private long startRealTime;
+    private long numEmitted;
+    private long interval;
+    private final int minSleepTime;
+    private IAtomicLong lagTracker;
+
+    public RandomDataP(int itemsPerSecond, boolean cooperative, String lagTrackerName,
+                       int minSleepTime) {
+        this.itemsPerSecond = itemsPerSecond;
+        this.cooperative = cooperative;
+        this.lagTrackerName = lagTrackerName;
+        this.interval = SECONDS.toNanos(1) / itemsPerSecond;
+        this.minSleepTime = minSleepTime;
+    }
+
+    @Override
+    protected void init(@Nonnull Context context) throws Exception {
+        startNanoTime = System.nanoTime();
+        startRealTime = System.currentTimeMillis();
+        lagTracker = context.jetInstance().getHazelcastInstance().getAtomicLong(lagTrackerName);
+    }
+
+    @Override
+    public boolean complete() {
+        long nowNs = System.nanoTime();
+        long shouldHaveEmitted = NANOSECONDS.toSeconds((nowNs - startNanoTime) * itemsPerSecond);
+        lagTracker.set(shouldHaveEmitted - numEmitted);
+        if (numEmitted == shouldHaveEmitted) {
+            // this is what kafka connector after PR#442 does: if it has nothing to emit, it just returns
+            if (cooperative) {
+                return false;
+            }
+            // we've emitted enough, let's sleep for a while
+            LockSupport.parkNanos(Math.min(interval, minSleepTime));
+        }
+        for (; numEmitted < shouldHaveEmitted; numEmitted++) {
+            if (!tryEmit(entry(startRealTime + numEmitted * 1000 / itemsPerSecond, random.nextInt()))) {
+                break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isCooperative() {
+        return cooperative;
+    }
+}
